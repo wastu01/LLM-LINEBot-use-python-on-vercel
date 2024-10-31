bd7bff0bdb6a7ab3263d472d1eb953773f0e9c06
wastu01 <adt107118@gm.ntcu.edu.tw>
2024-10-31 09:04:26 +0800

    Update llm.py

--- api/llm.py
+++ api/llm.py
@@ -20,8 +20,8 @@ class ChatGPT:
 
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default="gpt-4o-mini")
-        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0))
+        self.model = os.getenv("OPENAI_MODEL", default="gpt-4o")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0.2))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=600))
 
     def get_response(self):
